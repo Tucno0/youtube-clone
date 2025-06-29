f185ae68b1104743eab6b9fff5e9dd92de4d88df
Jhampier Tucno <yantucnoperez@gmail.com>
2025-06-29 01:41:07 -0500

    Refactor video workflow routes to standardize error handling and update API calls to OpenAI. Changed base URLs and model names for consistency across description and title generation.

--- src/app/api/videos/workflows/title/route.ts
+++ src/app/api/videos/workflows/title/route.ts
@@ -1,7 +1,7 @@
-import { db } from "@/db";
-import { videos } from "@/db/schema";
-import { serve } from "@upstash/workflow/nextjs";
-import { and, eq } from "drizzle-orm";
+import { db } from '@/db';
+import { videos } from '@/db/schema';
+import { serve } from '@upstash/workflow/nextjs';
+import { and, eq } from 'drizzle-orm';
 
 // curl -X POST http://localhost:3000/api/videos/workflows/title
 
@@ -22,44 +22,44 @@ export const { POST } = serve(async (context) => {
   const input = context.requestPayload as InputType;
   const { videoId, userId } = input;
 
-  const video = await context.run("get-video", async () => {
+  const video = await context.run('get-video', async () => {
     const [existingVideo] = await db
       .select()
       .from(videos)
       .where(and(eq(videos.id, videoId), eq(videos.userId, userId)));
 
     if (!existingVideo) {
-      throw new Error("Video not found");
+      throw new Error('Video not found');
     }
 
     return existingVideo;
   });
 
-  const transcript = await context.run("get-transcript", async () => {
+  const transcript = await context.run('get-transcript', async () => {
     const trackUrl = `https://stream.mux.com/${video.muxPlaybackId}/text/${video.muxTrackId}.txt`;
     const response = await fetch(trackUrl);
     const text = response.text();
 
     if (!text) {
-      throw new Error("Bad request");
+      throw new Error('Bad request');
     }
 
     return text;
   });
 
-  const { body } = await context.api.openai.call("generate-title", {
-    baseURL: "https://api.deepseek.com",
-    token: process.env.DEEPSEEK_API_KEY!,
-    operation: "chat.completions.create",
+  const { body } = await context.api.openai.call('generate-title', {
+    baseURL: 'https://api.openai.com',
+    token: process.env.OPENAI_API_KEY!,
+    operation: 'chat.completions.create',
     body: {
-      model: "deepseek-chat",
+      model: 'gpt-4o-mini',
       messages: [
         {
-          role: "system",
+          role: 'system',
           content: TITLE_SYSTEM_PROMPT,
         },
         {
-          role: "user",
+          role: 'user',
           content: transcript,
         },
       ],
@@ -69,10 +69,10 @@ export const { POST } = serve(async (context) => {
   const title = body.choices[0].message.content;
 
   if (!title) {
-    throw new Error("Bad request");
+    throw new Error('Bad request');
   }
 
-  await context.run("update-video", async () => {
+  await context.run('update-video', async () => {
     await db
       .update(videos)
       .set({
